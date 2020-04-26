7bce8bb75df8c0156d99877593b1e0254772abe5
fbjarkes <fbjarkes>
2020-04-26 18:18:59 +0200

    Better unittest for IB symbol parsing

--- qa_dataprovider/providers/async_ib_dataprovider.py
+++ qa_dataprovider/providers/async_ib_dataprovider.py
@@ -21,7 +21,8 @@ class AsyncIBDataProvider(GenericDataProvider):
     logger = logging.getLogger(__name__)
 
     def __init__(self, host: str = '127.0.0.1', port: int = 7496, timeout: int = 60, verbose: int = 0):
-        super(AsyncIBDataProvider, self).__init__(self.logger, verbose, chunk_size=20)
+        super(AsyncIBDataProvider, self).__init__(
+            self.logger, verbose, chunk_size=20)
         self.port = port
         self.host = host
         self.timeout = timeout
@@ -31,7 +32,8 @@ class AsyncIBDataProvider(GenericDataProvider):
         self.ib.disconnect()
 
     def connect(self):
-        self.ib.connect(self.host, self.port, clientId=int(random.uniform(1, 10000)), timeout=self.timeout, readonly=True)
+        self.ib.connect(self.host, self.port, clientId=int(
+            random.uniform(1, 10000)), timeout=self.timeout, readonly=True)
 
     def _initialize(self):
         self.connect()
@@ -51,7 +53,8 @@ class AsyncIBDataProvider(GenericDataProvider):
                 df_list.append(self._get_data_internal(symbol_data))
             except Exception as exc:
                 traceback.print_exc(file=sys.stderr)
-                self.logger.warning("Skipping {}: error message: {}".format(symbol_data.symbol, exc))
+                self.logger.warning(
+                    "Skipping {}: error message: {}".format(symbol_data.symbol, exc))
 
         if not keep_alive:
             self.ib.disconnect()
@@ -67,7 +70,8 @@ class AsyncIBDataProvider(GenericDataProvider):
         self.logger.info(f"Getting symbol data: {symbol_data}")
 
         if symbol_data.timeframe == 'day':
-            symbol, bars = self.__get_daily(symbol_data.start, symbol_data.symbol, symbol_data.end)
+            symbol, bars = self.__get_daily(
+                symbol_data.start, symbol_data.symbol, symbol_data.end)
             symbol = symbol_data.symbol.split('-')[0]
             dataframe = self.__to_dataframe(bars)
 
@@ -83,7 +87,8 @@ class AsyncIBDataProvider(GenericDataProvider):
             else:
                 from_date = f"{(to_dt - timedelta(days=30)):%Y-%m-%d}"
 
-            symbol, bars = self.__get_intraday(symbol_data.symbol, symbol_data.end, duration, '1 hour', symbol_data.rth_only)
+            symbol, bars = self.__get_intraday(
+                symbol_data.symbol, symbol_data.end, duration, '1 hour', symbol_data.rth_only)
             symbol = symbol_data.symbol.split('-')[0]
             dataframe = self.__to_dataframe(bars, tz_fix=True)
 
@@ -99,7 +104,8 @@ class AsyncIBDataProvider(GenericDataProvider):
             else:
                 from_date = f"{(to_dt - timedelta(days=30)):%Y-%m-%d}"
 
-            symbol, bars = self.__get_intraday(symbol_data.symbol, symbol_data.end, duration, '5 mins', symbol_data.rth_only)
+            symbol, bars = self.__get_intraday(
+                symbol_data.symbol, symbol_data.end, duration, '5 mins', symbol_data.rth_only)
             symbol = symbol_data.symbol.split('-')[0]
             dataframe = self.__to_dataframe(bars, tz_fix=True)
         else:
@@ -109,14 +115,14 @@ class AsyncIBDataProvider(GenericDataProvider):
         if dataframe.empty:
             self.logger.warning(f"Got empty df for {symbol_data}")
         else:
-            df = self._post_process(dataframe, symbol, symbol_data.start, symbol_data.end, symbol_data.timeframe, symbol_data.transform)
+            df = self._post_process(dataframe, symbol, symbol_data.start,
+                                    symbol_data.end, symbol_data.timeframe, symbol_data.transform)
 
         return df
 
     @staticmethod
-    def exctract_symbol(ticker: str, type: str='STK', exchange: str='ARCA',
-                        currency: str= 'USD', expire='', multiplier='') -> tuple:
-
+    def exctract_symbol(ticker: str, type: str = 'STK', exchange: str = 'ARCA',
+                        currency: str = 'USD', expire='', multiplier='') -> tuple:
         if ticker.count('-') == 4:
             symbol, type, exchange, currency, multiplier = ticker.split('-')
             if type.isdigit():
@@ -132,7 +138,7 @@ class AsyncIBDataProvider(GenericDataProvider):
         elif ticker.count('-') == 2:
             if ticker.find('CASH') > -1:
                 symbol, currency, exchange = ticker.split('-')
-                symbol = symbol.replace('.','')
+                symbol = symbol.replace('.', '')
                 type = 'FX'
             else:
                 a, b, c = ticker.split('-')
@@ -157,51 +163,51 @@ class AsyncIBDataProvider(GenericDataProvider):
     def parse_contract(ticker):
         """
         Backtrader contract specification (https://www.backtrader.com/docu/live/ib/ib.html):
-        
+
         TICKER # Stock type and SMART exchange
-        
+
         TICKER-STK # Stock and SMART exchange
-        
+
         TICKER-STK-EXCHANGE # Stock
-        
+
         TICKER-STK-EXCHANGE-CURRENCY # Stock
-        
+
         TICKER-CFD # CFD and SMART exchange
-        
+
         TICKER-CFD-EXCHANGE # CFD
-        
+
         TICKER-CDF-EXCHANGE-CURRENCY # Stock
-        
+
         TICKER-IND-EXCHANGE # Index
-        
+
         TICKER-IND-EXCHANGE-CURRENCY # Index
-        
+
         TICKER-YYYYMM-EXCHANGE # Future
-        
+
         TICKER-YYYYMM-EXCHANGE-CURRENCY # Future
-        
+
         TICKER-YYYYMM-EXCHANGE-CURRENCY-MULT # Future
-        
+
         TICKER-FUT-EXCHANGE-CURRENCY-YYYYMM-MULT # Future
-        
+
         TICKER-YYYYMM-EXCHANGE-CURRENCY-STRIKE-RIGHT # FOP
-        
+
         TICKER-YYYYMM-EXCHANGE-CURRENCY-STRIKE-RIGHT-MULT # FOP
-        
+
         TICKER-FOP-EXCHANGE-CURRENCY-YYYYMM-STRIKE-RIGHT # FOP
-        
+
         TICKER-FOP-EXCHANGE-CURRENCY-YYYYMM-STRIKE-RIGHT-MULT # FOP
-        
+
         CUR1.CUR2-CASH-IDEALPRO # Forex
-        
+
         TICKER-YYYYMMDD-EXCHANGE-CURRENCY-STRIKE-RIGHT # OPT
-        
+
         TICKER-YYYYMMDD-EXCHANGE-CURRENCY-STRIKE-RIGHT-MULT # OPT
-        
+
         TICKER-OPT-EXCHANGE-CURRENCY-YYYYMMDD-STRIKE-RIGHT # OPT
-        
+
         TICKER-OPT-EXCHANGE-CURRENCY-YYYYMMDD-STRIKE-RIGHT-MULT # OPT
-        
+
         :return: 
         """
         contract_type, symbol, exchange, currency, expire, multiplier = \
@@ -216,12 +222,12 @@ class AsyncIBDataProvider(GenericDataProvider):
         else:
             return Stock(symbol, exchange, currency)
 
-
-    def __get_intraday(self, ticker: str, to_date: str,duration: str,
-                       barsize:str, rth_only: bool) -> (str, [BarData]):
+    def __get_intraday(self, ticker: str, to_date: str, duration: str,
+                       barsize: str, rth_only: bool) -> (str, [BarData]):
         to_dt = datetime.strptime(f"{to_date} 11:59", '%Y-%m-%d %H:%M')
         contract = AsyncIBDataProvider.parse_contract(ticker)
-        whatToShow = 'MIDPOINT' if isinstance(contract, (Forex, CFD, Commodity)) else 'TRADES'
+        whatToShow = 'MIDPOINT' if isinstance(
+            contract, (Forex, CFD, Commodity)) else 'TRADES'
         bars = self.ib.reqHistoricalData(contract, endDateTime=to_dt, durationStr=duration,
                                          barSizeSetting=barsize,
                                          whatToShow=whatToShow,
@@ -229,8 +235,7 @@ class AsyncIBDataProvider(GenericDataProvider):
                                          formatDate=2)
         return contract.symbol, bars
 
-
-    def __get_daily(self, from_date: str, ticker:str , to_date:str) -> (str, [BarData]):
+    def __get_daily(self, from_date: str, ticker: str, to_date: str) -> (str, [BarData]):
         from_dt = datetime.strptime(from_date, "%Y-%m-%d")
         today = datetime.strptime(to_date, "%Y-%m-%d")
         to_dt = datetime(today.year, today.month, today.day, 23, 59, 59)
@@ -244,7 +249,8 @@ class AsyncIBDataProvider(GenericDataProvider):
             to_dt = None
 
         contract = AsyncIBDataProvider.parse_contract(ticker)
-        whatToShow = 'MIDPOINT' if isinstance(contract, (Forex, CFD, Commodity)) else 'TRADES'
+        whatToShow = 'MIDPOINT' if isinstance(
+            contract, (Forex, CFD, Commodity)) else 'TRADES'
         # bars = self.ib.reqDailyBars(contract, 2016)
         bars = self.ib.reqHistoricalData(contract, endDateTime=to_dt, durationStr=F"{days} D",
                                          barSizeSetting='1 day',
@@ -255,9 +261,9 @@ class AsyncIBDataProvider(GenericDataProvider):
 
     def __to_dataframe(self, bars, tz_fix=False):
         if tz_fix:
-            data = [{'Date':pd.to_datetime(b.date.astimezone(self.us_eastern).replace(tzinfo=None)),
-                     'Open': b.open, 'High': b.high, 'Low': b.low, 'Close':b.close,
-                     'Volume':b.volume} for b in bars]
+            data = [{'Date': pd.to_datetime(b.date.astimezone(self.us_eastern).replace(tzinfo=None)),
+                     'Open': b.open, 'High': b.high, 'Low': b.low, 'Close': b.close,
+                     'Volume': b.volume} for b in bars]
         else:
             data = [
                 {'Date': pd.to_datetime(b.date),
@@ -269,17 +275,18 @@ class AsyncIBDataProvider(GenericDataProvider):
         else:
             return pd.DataFrame()
 
-
     def add_quotes(self, data, ticker):
         return data
+
 
 if __name__ == '__main__':
     ib = AsyncIBDataProvider()
     #df_list = ib.get_data(['JBL'], '2020-03-01', '2020-04-08', timeframe='60min', transform='60min')
-    df_list = ib.get_data(['JBL'], '2020-03-01', '2020-04-08', timeframe='day', transform='day')
+    df_list = ib.get_data(['JBL'], '2020-03-01',
+                          '2020-04-08', timeframe='day', transform='day')
 
     for df in df_list:
         print(df.head())
         print(df.tail())
-    #print(dailys[0].tail())
-    #print(dailys[1].tail())
+    # print(dailys[0].tail())
+    # print(dailys[1].tail())
--- tests/test_ib.py
+++ tests/test_ib.py
@@ -1,44 +1,19 @@
 import unittest
 
-from ib_insync import Stock, Index, Forex, Future
 from qa_dataprovider.providers.async_ib_dataprovider import AsyncIBDataProvider
 
 
 class TestIb(unittest.TestCase):
 
-    def assert_contract(self, contract,
-                        expect_contract_type, expect_ticker,
-                        expect_exchange, expect_currency,
-                        expect_expire, multiplier=None):
-        assert isinstance(contract, expect_contract_type)
-        print(contract)
-        assert contract.symbol == expect_ticker
-        assert contract.exchange == expect_exchange
-        assert contract.currency == expect_currency
-        if expect_expire:
-            assert contract.lastTradeDateOrContractMonth == expect_expire
-        if multiplier:
-            assert contract.multiplier == multiplier
-
     def test_parse_contract(self):
-        self.assert_contract(AsyncIBDataProvider.parse_contract("SPY"),
-                             Stock, "SPY","ARCA","USD","")
-        self.assert_contract(AsyncIBDataProvider.parse_contract("BA-NYSE"),
-                             Stock, "BA", "NYSE", "USD","")
-        self.assert_contract(AsyncIBDataProvider.parse_contract("HM.B-SFB-SEK"),
-                             Stock, "HM.B", "SFB", "SEK","")
-        self.assert_contract(AsyncIBDataProvider.parse_contract("OMXS30-IND-OMS-SEK"),
-                             Index, "OMXS30", "OMS", "SEK", "")
-        self.assert_contract(AsyncIBDataProvider.parse_contract("USD.JPY-CASH-IDEALPRO"),
-                             Forex, "USD", "IDEALPRO", "JPY", "")
-        self.assert_contract(AsyncIBDataProvider.parse_contract("ES-201709-GLOBEX"),
-                             Future, "ES", "GLOBEX", "USD", "201709")
-        self.assert_contract(AsyncIBDataProvider.parse_contract("OMXS30-201708-OMS-SEK"),
-                             Future, "OMXS30", "OMS", "SEK", "201708")
-        self.assert_contract(AsyncIBDataProvider.parse_contract("DAX-201709-DTB-EUR-25"),
-                             Future, "DAX", "DTB", "EUR", "201709", "25")
-
-
+        assert AsyncIBDataProvider.exctract_symbol('SPY') == tuple(['STK', 'SPY', 'ARCA', 'USD', '', ''])
+        assert AsyncIBDataProvider.exctract_symbol('BA-NYSE') == tuple(['STK', 'BA', 'NYSE', 'USD', '', ''])
+        assert AsyncIBDataProvider.exctract_symbol('HM.B-SFB-SEK') == tuple(['STK', 'HM.B', 'SFB', 'SEK', '', ''])
+        assert AsyncIBDataProvider.exctract_symbol('OMXS30-IND-OMS-SEK') == tuple(['IND', 'OMXS30', 'OMS', 'SEK', '', ''])
+        assert AsyncIBDataProvider.exctract_symbol('USD.JPY-CASH-IDEALPRO') == tuple(['FX', 'USDJPY', 'IDEALPRO', 'CASH', '', ''])
+        assert AsyncIBDataProvider.exctract_symbol('ES-201709-GLOBEX') == tuple(['FUT', 'ES', 'GLOBEX', 'USD', '201709', ''])
+        assert AsyncIBDataProvider.exctract_symbol('OMXS30-201708-OMS-SEK') == tuple(['FUT', 'OMXS30', 'OMS', 'SEK', '201708', ''])
+        assert AsyncIBDataProvider.exctract_symbol('DAX-201709-DTB-EUR-25') == tuple(['FUT', 'DAX', 'DTB', 'EUR', '201709', '25'])
 
 
 if __name__ == '__main__':
